e25becc59a64b2aedcd5eb7e8f2a519932799247
maugern <n-mauger@hotmail.fr>
2017-02-04 19:16:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Projet Java [![Build Status](https://travis-ci.com/maugern/projetjava.svg?token=hCzmvv7J52L28FTDFzh8&branch=master)](https://travis-ci.com/maugern/projetjava)
+
+**Notre projet Java/JEE de troisième année à l'ESPI Lille.**
+
+Liste des membres de l'équipe :
+- [BOROMBO Erwan](https://github.com/BBorombo)
+- [CATTELLE Clément](https://github.com/komanokami)
+- [MAUGER Nicolas](https://github.com/maugern)
+
+
+## Lancer l'application en locale :
+
+Clonez d'abord le projet dans votre workspace :
+```sh
+git clone https://github.com/maugern/projetjava.git
+```
+
+Lancez ensuite l'application en locale :
+```sh
+mvn jetty:run
+```
+
+C'est fini! Vous pouvez à présent vous rendre sur l'adresse localhost:8080 pour voir le résultat.
+Si vous rencontrez une difficulté n'hésitez pas à [nous remonter le problème](https://github.com/maugern/projetjava/issues).
---
+++ src/main/java/api/User.java
@@ -0,0 +1,107 @@
+package api;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * User class
+ */
+@Entity
+public class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String alias;
+	private String name;
+	private String email;
+	private String passwdHash;
+	
+	/**
+	 * User constructor.
+	 * @param alias login or username, who must be unique
+	 * @param name label
+	 * @param email who must be unique
+	 * @param password non crypted
+	 */
+	public User(String alias,
+				String name,
+				String email,
+				String password)
+	{
+		this.alias = alias;
+		this.name = name;
+		this.email = email;
+		setPassword(password);
+	};
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * Set a new passwordHash using 
+	 * <a href="https://en.wikipedia.org/wiki/Bcrypt">bCrypt algorithm</a>.
+	 * Only hashed password will be save and no salt,
+	 * to protect against rainbow attacks.
+	 * @param password non crypted
+	 */
+	public void setPassword(String password) {
+		this.passwdHash = BCrypt.hashpw(password, generateSalt());
+	}
+
+	/**
+	 * Testing if password match hashed password using bCrypt algorithm. 
+	 * @param password to test
+	 * @return true if password match
+	 */
+	public boolean isGoodPassword(String password) {
+		return BCrypt.checkpw(password, this.passwdHash);
+	}
+
+	public String getPasswdHash() {
+		return passwdHash;
+	}
+
+	/** 
+	 * GenerateSalt log_rounds parameter determines the complexity
+	 * the work factor is 2**log_rounds, and the default is 12.
+	 * Always remember in computer science, is only a pseudo-random number.
+	 */
+	private String generateSalt() {
+		return BCrypt.gensalt(12); // increase default values, who is 10.
+	}
+	
+	@Override
+	public String toString() {
+		return "<" + id + "," + alias + "," + email + ">";
+	}
+	
+}
