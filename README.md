d64b26e8531b891105b97682417fbfaaca2158e4
Nicolas Mauger <n-mauger@hotmail.fr>
2017-02-14 11:50:56 +0100

    change projet name to QRcode-factory

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Projet Java [![Build Status](https://travis-ci.com/maugern/projetjava.svg?token=hCzmvv7J52L28FTDFzh8&branch=master)](https://travis-ci.com/maugern/projetjava)
+# QRcode Factory [![Build Status](https://travis-ci.org/maugern/QRcode-factory.svg?branch=master)](https://travis-ci.org/maugern/QRcode-factory)
 
 **Notre projet Java/JEE de troisième année à l'ESPI Lille.**
+
+Le sujet et la documentation est disponible sur [notre wiki](https://github.com/maugern/QRcode-factory/wiki).
 
 Liste des membres de l'équipe :
 - [BOROMBO Erwan](https://github.com/BBorombo)
@@ -12,13 +14,19 @@ Liste des membres de l'équipe :
 
 Clonez d'abord le projet dans votre workspace :
 ```sh
-git clone https://github.com/maugern/projetjava.git
+git clone https://github.com/maugern/QRcode-factory.git
 ```
 
-Lancez ensuite l'application en locale :
+Puis installer l'application via maven :
+```sh
+mvn install
+mvn compile
+```
+
+Enfin, lancez ensuite l'application en locale sur le port 8080 :
 ```sh
 mvn jetty:run
 ```
 
 C'est fini! Vous pouvez à présent vous rendre sur l'adresse localhost:8080 pour voir le résultat.
-Si vous rencontrez une difficulté n'hésitez pas à [nous remonter le problème](https://github.com/maugern/projetjava/issues).
+Si vous rencontrez une difficulté n'hésitez pas à [nous remonter le problème](https://github.com/maugern/QRcode-factory/issues).
--- src/test/java/api/QrCodeTest.java
+++ src/test/java/api/QrCodeTest.java
@@ -10,21 +10,36 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 public class QrCodeTest {
 	
-	@Rule
-	public TemporaryFolder folder= new TemporaryFolder();
+	@Rule public TemporaryFolder folder= new TemporaryFolder();
+	public File createdFolder;
+	public File file_to_test;
+
+	@Before
+	public void init_temporary_folder() throws  IOException {
+		createdFolder = folder.newFolder("junit_qrcode");
+		System.out.println("Creat temporary folder in " + createdFolder.getAbsolutePath());
+	}
 
 	@Test
 	public void should_generate_and_save_qrcode() throws IOException {
-		File createdFolder= folder.newFolder("junit_qrcode");
 		QrCode.generateAndSave("https://www.april.org/", 400, "png", createdFolder.getAbsolutePath() + System.getProperty("file.separator") + "qrcode.png");
+		System.out.println("A test qrccode will be create in Testing file will be create in : ");
 		System.out.println(createdFolder.getAbsolutePath() + System.getProperty("file.separator") + "qrcode.png");
-		File file_to_test = new File(createdFolder.getAbsolutePath() + System.getProperty("file.separator") + "qrcode.png");
+		file_to_test = new File(createdFolder.getAbsolutePath() + System.getProperty("file.separator") + "qrcode.png");
 		assertTrue(file_to_test.exists());
 	}
+
+	@After
+	public void automaticly_destroy_temporary_files_and_folders() {
+		System.out.println("End of QrCodeTest. Delete all temporary files");
+	}
+
 }
