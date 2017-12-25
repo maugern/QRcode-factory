d7ff13dc08df527c86d7b1c4516486edbee3d569
contact@maugern.fr <contact@maugern.fr>
2017-12-25 02:31:44 +0100

    correct typo in variable name

--- src/main/java/fr/maugern/validator/ValidationConstants.java
+++ src/main/java/fr/maugern/validator/ValidationConstants.java
@@ -10,10 +10,10 @@ public class ValidationConstants {
 
     private ValidationConstants(){}
 
-    protected static final int MIN_PASSWORD_LENGHT = 8;
+    protected static final int MIN_PASSWORD_LENGTH = 8;
     
-    protected static final int MIN_USERNAME_LENGHT = 8;
-    protected static final int MAX_USERNAME_LENGHT = 32;
+    protected static final int MIN_USERNAME_LENGTH = 8;
+    protected static final int MAX_USERNAME_LENGTH = 32;
     
     protected static final int MAX_URL_LENGTH = 4000; 
     protected static final String REGEX_URI = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
