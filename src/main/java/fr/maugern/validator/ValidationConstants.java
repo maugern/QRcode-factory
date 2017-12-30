a62c9bafa433555d59564edf45ad540f6b211856
Nicolas Mauger <contact@maugern.fr>
2017-12-30 01:02:47 +0100

    make ValidationConstants public to sync them with database values

--- src/main/java/fr/maugern/validator/ValidationConstants.java
+++ src/main/java/fr/maugern/validator/ValidationConstants.java
@@ -1,21 +1,20 @@
 /**
  * ValidationConstants.java
+ *
  * @author Nicolas
  * Created on 24 sept. 2017
  */
 package fr.maugern.validator;
 
-/** Validation values used in application's from */
+/**
+ * Validation values used in application's from
+ */
 public class ValidationConstants {
 
-    private ValidationConstants(){}
+    public static final int MIN_PASSWORD_LENGTH = 8;
+    public static final int MIN_USERNAME_LENGTH = 8;
+    public static final int MAX_USERNAME_LENGTH = 32;
+    public static final int MAX_URL_LENGTH = 4000;
+    public static final String REGEX_URI = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
 
-    protected static final int MIN_PASSWORD_LENGTH = 8;
-    
-    protected static final int MIN_USERNAME_LENGTH = 8;
-    protected static final int MAX_USERNAME_LENGTH = 32;
-    
-    protected static final int MAX_URL_LENGTH = 4000; 
-    protected static final String REGEX_URI = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-    
 }
